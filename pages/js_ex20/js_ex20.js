51db8bb442fa820783d77caef19ff8506884835a
ShaheerSharif <sharfshaheer@gmail.com>
2023-04-08 08:46:56 +0500

    2nd commit

---
+++ pages/js_ex20/js_ex20.js
@@ -0,0 +1,9 @@
+const lName = input('Enter last name : ')
+const lNameLower = lName.toLowerCase()
+
+if (lNameLower === 'shaheer') {
+  alert('Logged in');
+}
+else {
+  alert('Failed to log in')
+}
